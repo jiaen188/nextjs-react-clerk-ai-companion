616741662da1fda7154b7720b1af4f42b9cd833e
gulifan <gulifan@dxy.cn>
2024-03-03 22:12:38 +0800

    feat: 路由组初始化

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+  return <div>login page (unprotected)</div>;
+};
+
+export default LoginPage;
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,5 @@
+const RootPage = () => {
+  return <div>root page (protected)</div>;
+};
+
+export default RootPage;
