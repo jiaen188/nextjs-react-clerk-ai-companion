fa134e458c700888f2f5a9e3f4466e4556492b70
gulifan <gulifan@dxy.cn>
2024-03-05 00:24:46 +0800

    feat: 接入clerk 鉴权

--- app/(auth)/(routes)/login/page.tsx
+++
@@ -1,5 +0,0 @@
-const LoginPage = () => {
-  return <div>login page (unprotected)</div>;
-};
-
-export default LoginPage;
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,5 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 const RootPage = () => {
-  return <div>root page (protected)</div>;
+  return (
+    <div>
+      root page (protected) <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default RootPage;
